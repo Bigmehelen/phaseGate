d36d9d754f42cc0d97af862ab0037652d807a5fe
Bigmehelen <getfitbyhelena@gmail.com>
2025-06-08 20:50:48 +0100

    second

--- PhaseGatejs/MainMenstralApp.js
+++ PhaseGatejs/MainMenstralApp.js
@@ -1,27 +1,27 @@
-import MenstralApp;
-from MenstralApp import *;
+const {tracker, expectingNextPeriod, checkRegularOrNot, fertileWindow} = require ('./menstralApps')
+
 const prompt = require("prompt-sync")();
 
-let previous = prompt("Enter previous menstrual day (dd-MM-yyyy): ");
-let next = prompt("Enter current menstrual day (dd-MM-yyyy): ");
+const previous = prompt("Enter previous menstrual day (dd-MM-yyyy): ");
+const next = prompt("Enter current menstrual day (dd-MM-yyyy): ");
 console.log("You have a " + tracker(previous, next) + " day cycle");
 
-let lastPeriod = prompt("Enter last period date (dd-MM-yyyy): ");
-let cycleLength = Number(prompt("Enter cycle length: "), 10);
-console.log("Your next period is on " + expectingNextPeriod(lastPeriod, cycleLength) + "\n");
+const lastPeriod = prompt("Enter last period date (dd-MM-yyyy): ");
+const cycleLength = Number(prompt("Enter cycle length: "), 10);
+console.log("Your next period is on " + expectingNextPeriod(lastPeriod, cycleLength));
 
-let numDates = Number(prompt("Enter number of period dates to check regularity: "), 10);
-let dates = [];
+const numDates = Number(prompt("Enter number of period dates to check regularity: "), 10);
+const dates = [];
 
 for (let i = 0; i < numDates; i++) {
 	dates.push(prompt(`Enter period date ${i + 1} (dd-MM-yyyy):`));
 }
 
-let result = checkRegularOrNot(dates, cycleLength);
-console.log("Period regularity: " + result + "\n");
+const result = checkRegularOrNot(dates, cycleLength);
+console.log("Period regularity: " + result);
 
-let startDay = prompt("Enter start date (dd-MM-yyyy):");
-let noOfDays = parseInt(prompt("Enter cycle length:"), 10);
-let results = fertileWindow(startDay, noOfDays);
+const startDay = prompt("Enter start date (dd-MM-yyyy):");
+const noOfDays = parseInt(prompt("Enter cycle length:"), 10);
+const results = fertileWindow(startDay, noOfDays);
 console.log(results);
 
